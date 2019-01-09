900a05f17562320fad240f739d08cf0e73b0805b
pratyakash <pratyakash13@gmail.com>
2019-01-09 22:04:53 +0530

    Updates

---
+++ precache-manifest.f7f65bc71210b91358f91e1189c84162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "6974a77f32bf6e5e74e4",
+    "url": "/weather/static/js/main.6974a77f.chunk.js"
+  },
+  {
+    "revision": "babd94826ba891d51a02",
+    "url": "/weather/static/js/1.babd9482.chunk.js"
+  },
+  {
+    "revision": "6974a77f32bf6e5e74e4",
+    "url": "/weather/static/css/main.5272ac27.chunk.css"
+  },
+  {
+    "revision": "8e21913633abf700d4f0c6cfbbd97de9",
+    "url": "/weather/index.html"
+  }
+];
